5c2cd44af48a3b25499ccfcbcf85e7d74c20155e
Joshua Wang <jwangc2@gmail.com>
2015-04-26 01:59:55 -0400

    Added in-game controls and interactions

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public TriggerCheck groundCheck;
+    public float hspd = 10;
+    public string hAxis;
+    public string jumpButton;
+    public string attackButton;
+    private Rigidbody2D rig;
+    private Animator anim;
+    private float h;
+
+	// Use this for initialization
+	void Start () {
+        rig = this.gameObject.GetComponent<Rigidbody2D>();
+        anim = this.gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        anim.SetInteger("h", (int)h);
+        anim.SetBool("OnGround", OnGround());
+        anim.SetBool("Attack", Input.GetButton(attackButton));
+	}
+
+    void FixedUpdate() {
+        if (OnGround())
+        {
+            // On the ground
+            h = Input.GetAxisRaw(hAxis);
+            if (h != 0)
+            {
+                rig.velocity = new Vector2(rig.gravityScale * h * hspd, rig.velocity.y);
+                transform.localScale = new Vector3(h, transform.localScale.y);
+            }
+
+            if (Input.GetButtonDown(jumpButton)) {
+                rig.velocity = new Vector2(rig.velocity.x, rig.gravityScale * 3.5f);
+            }
+        }
+
+    }
+
+    bool OnGround() {
+        if (groundCheck) {
+            return groundCheck.hit != null;
+        }
+
+        return false;
+    }
+
+}
---
+++ Assets/Scripts/TriggerCheck.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerCheck : MonoBehaviour {
+
+    public Collider2D hit = null;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other) {
+        hit = other;
+    }
+
+    void OnTriggerStay2D(Collider2D other) {
+        hit = other;
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        hit = null;
+    }
+}
